09deae3779ede9e54f347f00ea6e848cbddcbdf2
Alexander Infantino <infantinoalex@gmail.com>
2018-12-12 20:55:57 -0500

    Uncommenting code for presentaion

--- SemesterPartTwo/Program.cs
+++ SemesterPartTwo/Program.cs
@@ -10,146 +10,115 @@ namespace SemesterPartTwo
     {
         static void Main(string[] args)
         {
-            //Console.WriteLine("Testing out CFG Creation");
+            Console.WriteLine("Testing out CFG Creation");
 
-            //var inClassCFG = CreateCFGs.CreateInClassCFG();
-            //for (int counter = 0; counter < 5; counter++)
-            //{
-            //    var result = inClassCFG.CalculateStringsInLanguageOfDepth(counter);
-            //    result.Reverse();
-            //    Console.WriteLine($"Strings in the language of depth: {counter} are: {string.Join(",", result)}");
-            //}
+            var inClassCFG = CreateCFGs.CreateInClassCFG();
+            for (int counter = 0; counter < 5; counter++)
+            {
+                var result = inClassCFG.CalculateStringsInLanguageOfDepth(counter);
+                result.Reverse();
+                Console.WriteLine($"Strings in the language of depth: {counter} are: {string.Join(",", result)}");
+            }
 
-            //for (int counter = 0; counter < 10; counter++)
-            //{
-            //    var result = inClassCFG.RandomStringFromCFG(100);
-            //    Console.WriteLine($"Random string from the language: {result}");
-            //}
+            for (int counter = 0; counter < 10; counter++)
+            {
+                var result = inClassCFG.RandomStringFromCFG(100);
+                Console.WriteLine($"Random string from the language: {result}");
+            }
 
-            //Console.WriteLine("Done. Press [Enter] to continue.");
-            //Console.ReadLine();
+            Console.WriteLine("Done. Press [Enter] to continue.");
+            Console.ReadLine();
 
-            //Console.WriteLine("Testing Chris's CFG to see if we get similar results");
-            //var chrisCFG = CreateCFGs.CreateChrisCFG();
-            //for (int counter = 0; counter < 10; counter++)
-            //{
-            //    var result = chrisCFG.CalculateStringsInLanguageOfDepth(counter);
-            //    result.Reverse();
-            //    Console.WriteLine($"Strings in the language of depth: {counter} are: {string.Join(",", result)}");
-            //}
+            Console.WriteLine("Checking PDA");
+            var pda = CreatePDA.Create0N1NPDA();
+            foreach (var word in WordsToCheck0N1N())
+            {
+                var result = pda.Execute(word);
+                var acceptedRejectedString = result ? "accepted" : "rejected";
+                Console.WriteLine($"Word: {word} has been {acceptedRejectedString}");
+            }
 
-            //for (int counter = 0; counter < 10; counter++)
-            //{
-            //    var result = chrisCFG.RandomStringFromCFG(25);
-            //    Console.WriteLine($"Random string from the language: {result}");
-            //}
+            Console.WriteLine("Done. Press [Enter] to continue");
+            Console.ReadLine();
 
-            //Console.WriteLine("Done. Press [Enter] to continue");
-            //Console.ReadLine();
+            Console.WriteLine("Checking TM");
+            var inClassTM = CreateTM.CreateInClassTM();
 
-            //Console.WriteLine("Checking PDA");
-            //var pda = CreatePDA.Create0N1NPDA();
-            //foreach (var word in WordsToCheck0N1N())
-            //{
-            //    var result = pda.Execute(word);
-            //    var acceptedRejectedString = result ? "accepted" : "rejected";
-            //    Console.WriteLine($"Word: {word} has been {acceptedRejectedString}");
-            //}
+            foreach (var word in WordsToCheckForInClassTM())
+            {
+                var result = inClassTM.Execute(word);
+                var acceptedRejectedString = result ? "accepted" : "rejected";
+                Console.WriteLine($"Word: {word} has been {acceptedRejectedString}");
+            }
 
-            //Console.WriteLine("Done. Press [Enter] to continue");
-            //Console.ReadLine();
+            Console.WriteLine("Done. Press [Enter] to continue");
+            Console.ReadLine();
 
-            //Console.WriteLine("Checking TM");
-            //var inClassTM = CreateTM.CreateInClassTM();
+            Console.WriteLine("Checking TM");
+            var inBookTM = CreateTM.Create0WhoseLengthIsPowerTwo();
 
-            //foreach (var word in WordsToCheckForInClassTM())
-            //{
-            //    var result = inClassTM.Execute(word);
-            //    var acceptedRejectedString = result ? "accepted" : "rejected";
-            //    Console.WriteLine($"Word: {word} has been {acceptedRejectedString}");
-            //}
+            foreach (var word in WordsToCheckForInBookTM())
+            {
+                var result = inBookTM.Execute(word);
+                var acceptedRejectedString = result ? "accepted" : "rejected";
+                Console.WriteLine($"Word: {word} has been {acceptedRejectedString}");
+            }
 
-            //Console.WriteLine("Done. Press [Enter] to continue");
-            //Console.ReadLine();
+            Console.WriteLine("Done. Press [Enter] to continue");
+            Console.ReadLine();
 
-            //Console.WriteLine("Checking TM");
-            //var inBookTM = CreateTM.Create0WhoseLengthIsPowerTwo();
+            Console.WriteLine("Checking ND TM");
+            var ndTM = CreateTM.CreateInClassNDTM();
+            var upperLimit = 100;
 
-            //foreach (var word in WordsToCheckForInBookTM())
-            //{
-            //    var result = inBookTM.Execute(word);
-            //    var acceptedRejectedString = result ? "accepted" : "rejected";
-            //    Console.WriteLine($"Word: {word} has been {acceptedRejectedString}");
-            //}
+            foreach (var word in WordsToCheckForInClassTM())
+            {
+                var result = ndTM.Execute(word, upperLimit);
+                var acceptedRejectedString = result ? "accepted" : "rejected";
+                Console.WriteLine($"Word: {word} has been {acceptedRejectedString}");
+            }
 
-            //Console.WriteLine("Done. Press [Enter] to continue");
-            //Console.ReadLine();
+            Console.WriteLine("Done. Press [Enter] to continue");
+            Console.ReadLine();
 
-            //Console.WriteLine("Checking ND TM");
-            //var ndTM = CreateTM.CreateInClassNDTM();
-            //var upperLimit = 100;
+            Console.WriteLine("Checking Linearly Bounded TM");
+            var inClassTMBounded = CreateTM.CreateInClassTMLinearlyBound();
 
-            //foreach (var word in WordsToCheckForInClassTM())
-            //{
-            //    var result = ndTM.Execute(word, upperLimit);
-            //    var acceptedRejectedString = result ? "accepted" : "rejected";
-            //    Console.WriteLine($"Word: {word} has been {acceptedRejectedString}");
-            //}
+            foreach (var word in WordsToCheckForInClassTM())
+            {
+                var result = inClassTMBounded.Execute(word);
+                var acceptedRejectedString = result ? "accepted" : "rejected";
+                Console.WriteLine($"Word: {word} has been {acceptedRejectedString}");
+            }
 
-            //Console.WriteLine("Done. Press [Enter] to continue");
-            //Console.ReadLine();
+            Console.WriteLine("Done. Press [Enter] to continue");
+            Console.ReadLine();
 
-            //Console.WriteLine("Checking Linearly Bounded TM");
-            //var inClassTMBounded = CreateTM.CreateInClassTMLinearlyBound();
+            Console.WriteLine("Checking Binary Increment TM");
+            var binaryIncrement = CreateTM.CreateBinaryIncrement();
 
-            //foreach (var word in WordsToCheckForInClassTM())
-            //{
-            //    var result = inClassTMBounded.Execute(word);
-            //    var acceptedRejectedString = result ? "accepted" : "rejected";
-            //    Console.WriteLine($"Word: {word} has been {acceptedRejectedString}");
-            //}
+            foreach (var word in BinaryIncrementWords())
+            {
+                var result = binaryIncrement.Execute(word);
+                var acceptedRejectedString = result ? "accepted" : "rejected";
+                Console.WriteLine($"Word: {word} has been {acceptedRejectedString}");
+            }
 
-            //Console.WriteLine("Done. Press [Enter] to continue");
-            //Console.ReadLine();
+            Console.WriteLine("Done. Press [Enter] to continue");
+            Console.ReadLine();
 
-            //Console.WriteLine("Checking Linearly Bounded TM");
-            //var failedLienarBounded = CreateTM.CreateFailedLinearBoundedTM();
+            Console.WriteLine("Checking Binary Decrement TM");
+            var binaryAddition = CreateTM.BinaryAdditionTM();
 
-            //foreach (var word in WordsToCheckForInClassTM())
-            //{
-            //    var result = failedLienarBounded.Execute(word);
-            //    var acceptedRejectedString = result ? "accepted" : "rejected";
-            //    Console.WriteLine($"Word: {word} has been {acceptedRejectedString}");
-            //}
+            foreach (var word in BinaryAdditionWords())
+            {
+                var result = binaryAddition.Execute(word);
+                var acceptedRejectedString = result ? "accepted" : "rejected";
+                Console.WriteLine($"Word: {word} has been {acceptedRejectedString}");
+            }
 
-            //Console.WriteLine("Done. Press [Enter] to continue");
-            //Console.ReadLine();
-
-            //Console.WriteLine("Checking Binary Increment TM");
-            //var binaryIncrement = CreateTM.CreateBinaryIncrement();
-
-            //foreach (var word in BinaryIncrementWords())
-            //{
-            //    var result = binaryIncrement.Execute(word);
-            //    var acceptedRejectedString = result ? "accepted" : "rejected";
-            //    Console.WriteLine($"Word: {word} has been {acceptedRejectedString}");
-            //}
-
-            //Console.WriteLine("Done. Press [Enter] to continue");
-            //Console.ReadLine();
-
-            //Console.WriteLine("Checking Binary Decrement TM");
-            //var binaryAddition = CreateTM.BinaryAdditionTM();
-
-            //foreach (var word in BinaryAdditionWords())
-            //{
-            //    var result = binaryAddition.Execute(word);
-            //    var acceptedRejectedString = result ? "accepted" : "rejected";
-            //    Console.WriteLine($"Word: {word} has been {acceptedRejectedString}");
-            //}
-
-            //Console.WriteLine("Done. Press [Enter] to continue");
-            //Console.ReadLine();
+            Console.WriteLine("Done. Press [Enter] to continue");
+            Console.ReadLine();
 
             Console.WriteLine("Checking MultiTape TM");
             var multiTape = CreateTM.CreateExampleMultiTapeTM();
@@ -157,6 +126,19 @@ namespace SemesterPartTwo
             foreach (var word in MultiTapeTMWords())
             {
                 var result = multiTape.Execute(word);
+                var acceptedRejectedString = result ? "accepted" : "rejected";
+                Console.WriteLine($"Word: {word} has been {acceptedRejectedString}");
+            }
+
+            Console.WriteLine("Done. Press [Enter] to continue");
+            Console.ReadLine();
+
+            Console.WriteLine("Checking Linearly Bounded TM");
+            var failedLienarBounded = CreateTM.CreateFailedLinearBoundedTM();
+
+            foreach (var word in WordsToCheckForInClassTM())
+            {
+                var result = failedLienarBounded.Execute(word);
                 var acceptedRejectedString = result ? "accepted" : "rejected";
                 Console.WriteLine($"Word: {word} has been {acceptedRejectedString}");
             }
